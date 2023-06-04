f1b08fa10f69025c3d0829e11463f92a6fbd5cd5
Millos <millosenriquejinete@hotmail.com>
2023-06-04 00:52:18 -0500

    practicing dom

--- domPractice.js
+++ domPractice.js
@@ -133,3 +133,199 @@ function mouseOver(id) {
 function mouseOut(id) {
     id.innerHTML = "<b>"+"Mouse Over me"+"</b>";
 };
+
+//NodeList
+//devuevle una lista estatica con todos los elementos que sean hijos directos de la etiqueta body
+console.log(document.querySelectorAll("body")); 
+
+//item method
+//permite acceder a un elemento del objeto NodeList devuelto a traves de su indice
+console.log(document.querySelectorAll("body").item(0));
+
+//forEach method
+//permite recorrer el objeto NodeList devuelto 
+                                    //(padre,hijo)    
+var list = document.querySelectorAll('body > *');
+list.forEach(function (element) {
+    //console.log(element);
+});
+
+//selectores 
+//encontrar el primer elemento que contenga la clase button
+console.log(document.querySelector(".button"));
+
+//encontrar todos los elementos con la clase button
+console.log(document.querySelectorAll(".button"));
+
+//querySelector method - devueleve el primer nodo o elemento que coincida con el selector proporcionado por parametro
+//tag name
+console.log(document.querySelector("h1"));
+//class name
+console.log(document.querySelector(".intro"));
+//id name
+console.log(document.querySelector("#message"));
+
+//querySelectorAll method - devuelve todos los nodos o elementos que coincidan con el selector proporcionado por parametro
+//tag name
+console.log(document.querySelectorAll("h2"));
+//class name
+console.log(document.querySelectorAll(".intro"));
+
+//DOMTokenList
+//length propertie
+const span =document.querySelector("span");
+// const classes1 = document.querySelector("span").classList; //mas corto
+const classes = span.classList; //retorna un objeto key:value con las clases del tag span
+const length = classes.length;
+span.textContent = `la longitud del objeto es:${length}`;
+
+//value propertie
+const value = classes.value;
+console.log(value); //retorna las clases del tag span
+//obtiene una clase especifica, funciona como un array
+console.log(classes[2]);
+console.log(document.querySelector("span").classList[2]);
+
+//add() method
+classes.add("d","e"); //agg nuevas clases
+console.log(classes);
+
+//contains() method - para saber si una clase pertenece o no al tag
+const contains = classes.contains("d"); //devuelve un booleano
+console.log(contains);
+
+const contains2 = classes.contains("a") 
+                ? "si contiene la clase a"
+                : "no contiene la clase a";
+console.log(contains2);
+
+//entries() method 
+//retorna un iterador permieitendo ir a traves de las key:value en el tag definido
+//los key:value los retorna como arrays cada uno por separado
+console.log(classes.entries());
+for (const i of classes.entries()) {
+    console.log(i); 
+}; 
+
+//item() method
+//retorna un item de la lista de clases determinado por su posicion en la lista con el indx
+console.log(classes.item(classes.length-1)); //retorna la ultima clase
+console.log(classes.item(2));//retorna la clase en la posicion 2
+
+//keys() method
+//retorna un iterador que permite ir a traves de los keys contenidos dentro de la clase del tag
+console.log(classes.keys());
+for (const i of classes.keys()) {
+    console.log(i);
+    console.log(classes.item(i)); //retorna los values de cada key que recorre i
+};
+
+//remove method
+//elimina la clase especifica de la lista de clases
+classes.remove("a","d");
+console.log(classes);
+
+//replace method
+//replace una clase existente con una nueva, si la clase que se va a reemplazar no existe
+//retorna un false y no agg la nueva clase a la lista
+classes.replace("b","h");
+console.log(classes);
+
+//operador ternario para hacer la validacion
+const replace = classes.replace("h","l") ? classes : "esa clase no existe";
+console.log(replace);
+
+//toggle() method
+//permite agg o quitar una clase concreta de una lista de clases, si la clase existe ejecutara un metodo remove
+//si la clase no existe ejecutara un metodo add
+const toggle = classes.toggle("i")
+            ? "added"
+            : "removed";
+console.log(toggle); //added
+console.log(classes);
+
+//values() method
+//retorna un iterador permitiendo ir a traves de los values contenidos en la clase
+//retorna los valores como strings
+for (const i of classes.values()) {
+    console.log(i);
+};
+
+//createElement method
+var label = document.createElement("label"); //crea un elemento html
+//propiedad ID
+label.id = "labelId"; //establece el nombre identificador unico en el documento
+//propiedad innerHTML
+console.log(document.body.innerHTML); // obtiene todo el body
+label.innerHTML = "Hola soy Millos"; //agg ese texto al label creado
+const test = document.getElementsByTagName("h3");
+const h3length = test[test.length-1]; //obtiene el ultimo h3
+h3length.append(label); //agg el label al h3
+console.log(h3length);
+console.log(label);
+
+//propiedad value
+//permite devolver o establecer el valor de un elemento de formulario del DOM.
+//solo esta disponible para elementos de formulario
+document.querySelector(".nombre").value = "hola world";
+console.log(document.querySelector(".nombre").value);
+
+//setAttribute method
+//permite establecer atributos a los elementos accesibles de Js
+const p = document.createElement("p");
+  //setAttribute(tipo[id,class],name);
+p.setAttribute("class","testClass");
+p.append("hola bro");
+document.getElementsByClassName("nombre")[0].append(p);
+console.log(p);
+
+//propiedad classList
+var body =document.body; //guarda todo el body
+body.classList.add("prueba");
+console.log(body);
+
+//previousElementSibling propiedad
+//devuelve el elemento que se encuentra justo antes del referenciado
+var elem = document.querySelector("#h3");
+console.log(elem.previousElementSibling); //<p>This example...
+
+//previousSibling - no se muy bien para que sirve
+console.log(elem.previousSibling);
+
+//nextElementSibling propiedad
+//esta propiedad devuelve el elemento que se encuentra justo despues del referenciado
+console.log(elem.nextElementSibling);
+console.log(elem.nextSibling);
+
+//parentElement propiedad
+//retorna el contenedor del elemento referenciado, es decir retorna su padre
+console.log(document.getElementById("testDate").parentElement);
+console.log(document.getElementById("well").parentElement); //<div id="main"></div>
+
+//parentNode
+console.log(document.getElementById("well").parentNode); //<div id="main">
+
+//append method
+//append acepta objects y DOMStrings
+//appendChild solo acepta objects
+const div = document.createElement("div");
+div.id = "divTest";
+const script = document.getElementById("script");
+script.parentElement.insertBefore(div,script); //lo agrega al final del body
+let lastDiv =document.getElementById("divTest"); //obtengo el div acabado de agg
+console.log(lastDiv);
+const pElem = document.createElement("p"); //creo un p
+p.textContent = "prueba append method"; //le agg texto
+
+lastDiv.appendChild(p); //agg el p al div previamente creado
+// The div would then look like this <div><p></p></div>
+
+lastDiv.append("apprending text...");
+// The div would then look like this <div>Appending Text</div>
+
+//insertBefore method
+const labe = document.createElement("label");
+labe.textContent = "yo soy millos";
+const h = document.getElementById("pr");
+h.parentElement.insertBefore(labe,h);
+
--- login/scriptLogin.js
+++ login/scriptLogin.js
@@ -27,3 +27,4 @@ function submitt() {
     
     //f.reset();
 };
+
